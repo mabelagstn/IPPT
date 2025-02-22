a88caaa6f605c354436774705f1d31a173f0b577
Mabel Augustine <mabel.augustine@u.nus.edu>
2025-02-22 16:35:24 +0800

    Update and rename mission06-template.py to IPPTScore.py

--- IPPTScore.py
+++ IPPTScore.py
@@ -1,23 +1,5 @@
-#
-# CS1010S --- Programming Methodology
-#
-# Mission 6
-#
-# Note that written answers are commented out to allow us to run your
-# code easily while grading your problem set.
-
 from ippt import *
 import csv
-
-##########
-# Task 1 #
-##########
-
-# Function read_csv has been given to help you read the csv file.
-# The function returns a tuple of tuples containing rows in the csv
-# file and its entries.
-
-# Alternatively, you may use your own method.
 
 def read_csv(csvfilename):
     rows = ()
@@ -47,27 +29,7 @@ def read_data(filename):
 pushup_table = read_data("pushup.csv")
 situp_table = read_data("situp.csv")
 run_table = read_data("run.csv")
-
 ippt_table = make_ippt_table(pushup_table, situp_table, run_table)
-
-# print("## Q1 ##")
-# Sit-up score of a 24-year-old who did 10 sit-ups.
-# print(access_cell(situp_table, 24, 10))    # 0
-
-# Push-up score of a 18-year-old who did 30 push-ups.
-# print(access_cell(pushup_table, 18, 30))   # 16
-
-# Run score of a 30-year old-who ran 12 minutes (720 seconds)
-# print(access_cell(run_table, 30, 720))     # 36
-
-# Since our run.csv file does not have data for 725 seconds, we should
-# get None if we try to access that cell.
-# print(access_cell(run_table, 30, 725))     # None
-
-
-##########
-# Task 2 #
-##########
 
 def pushup_score(pushup_table, age, pushup):
     if pushup > 60:
@@ -92,7 +54,6 @@ def run_score(run_table, age, run):
         run = 510
     return access_cell(run_table, age, run)
 
-# print("## Q2 ##")
 # print(pushup_score(pushup_table, 18, 61))   # 25
 # print(pushup_score(pushup_table, 18, 70))   # 25
 # print(situp_score(situp_table, 24, 0))      # 0
@@ -102,11 +63,6 @@ def run_score(run_table, age, run):
 # print(run_score(run_table, 30, 735))        # 35
 # print(run_score(run_table, 30, 500))        # 50
 # print(run_score(run_table, 30, 1300))       # 0
-
-
-##########
-# Task 3 #
-##########
 
 def ippt_award(score):
     if score < 51:
@@ -120,17 +76,11 @@ def ippt_award(score):
     else:
         return "G"
 
-# print("## Q3 ##")
 # print(ippt_award(50))     # F
 # print(ippt_award(51))     # P
 # print(ippt_award(61))     # P$
 # print(ippt_award(75))     # S
 # print(ippt_award(85))     # G
-
-
-##########
-# Task 4 #
-##########
 
 def ippt_results(ippt_table, age, pushup, situp, run):
     # (Total IPPT Score, IPPT Award String)
@@ -141,17 +91,11 @@ def ippt_results(ippt_table, age, pushup, situp, run):
     award = ippt_award(total)
     return (total, award)
 
-# print("## Q4 ##")
 # print(ippt_results(ippt_table, 25, 30, 25, 820))      # (53, 'P')
 # print(ippt_results(ippt_table, 28, 56, 60, 530))      # (99, 'G')
 # print(ippt_results(ippt_table, 38, 18, 16, 950))      # (36, 'F')
 # print(ippt_results(ippt_table, 25, 34, 35, 817))      # (61, 'P$')
 # print(ippt_results(ippt_table, 60, 70, 65, 450))      # (100, 'G')
-
-
-##########
-# Task 5 #
-##########
 
 def parse_results(csvfilename):
     data = []
@@ -169,23 +113,11 @@ def parse_results(csvfilename):
         data.append((name, age, pushup, situp, run, total, award))
     return data
 
-# print("## Q5 ##")
-# ippt_takers_data = parse_results("ippt_takers_data.csv")
-# print(ippt_takers_data[0])
-# print(ippt_takers_data[1])
-# print(ippt_takers_data[2])
-# print(ippt_takers_data[3])
-
-# Expected Output:
-# ('Name', 'Age', 'Push-Ups', 'Sit-Ups', '2.4-Km-Run', 'Total-Score', 'Award')
-# ('Sean Hendricks', 38, 25, 74, 1212, 42, 'F')
-# ('Phillip Brown DDS', 59, 15, 15, 852, 61, 'P$')
-# ('Ryan Gray MD', 24, 45, 78, 1074, 46, 'F')
-
-
-##########
-# Task 6 #
-##########
+ippt_takers_data = parse_results("ippt_takers_data.csv")
+# print(ippt_takers_data[0])    # ('Name', 'Age', 'Push-Ups', 'Sit-Ups', '2.4-Km-Run', 'Total-Score', 'Award')
+# print(ippt_takers_data[1])    # ('Sean Hendricks', 38, 25, 74, 1212, 42, 'F')
+# print(ippt_takers_data[2])    # ('Phillip Brown DDS', 59, 15, 15, 852, 61, 'P$')
+# print(ippt_takers_data[3])    # ('Ryan Gray MD', 24, 45, 78, 1074, 46, 'F')
 
 def num_awards(ippt_takers_data, age):
     res = {}
@@ -196,18 +128,8 @@ def num_awards(ippt_takers_data, age):
         res[row[-1]] += 1
     return res
 
-# print("## Q6 ##")
-# print(num_awards(ippt_takers_data, 25))
-# print(num_awards(ippt_takers_data, 28))
-
-# Expected Output:
-# {'F': 56, 'P': 20, 'G': 18, 'P$': 14, 'S': 10}
-# {'F': 54, 'S': 13, 'G': 16, 'P': 8, 'P$': 12}
-
-
-##########
-# Task 7 #
-##########
+# print(num_awards(ippt_takers_data, 25))    # {'F': 56, 'P': 20, 'G': 18, 'P$': 14, 'S': 10}
+# print(num_awards(ippt_takers_data, 28))    # {'F': 54, 'S': 13, 'G': 16, 'P': 8, 'P$': 12}
 
 def top_k_scores(ippt_takers_data, k, age):
     data = list(filter(lambda r : int(r[1]) == age, ippt_takers_data[1:]))
@@ -229,12 +151,6 @@ def top_k_scores(ippt_takers_data, k, age):
         k += 1 
     return result[:k]
 
-# print("## Q7 ##")
-# print(top_k_scores(ippt_takers_data, 5, 25))
-# print(top_k_scores(ippt_takers_data, 1, 28))
-# print(top_k_scores(ippt_takers_data, 2, 28))
-
-# Expected Output:
-# [('Joseph Burns', 100), ('Mike Williams', 98), ('Rachel Serrano', 98), ('Margaret Jennings', 97), ('Alexandra Day', 95), ('Stephen Boyer', 95)]
-# [('Eric Villegas', 100), ('Melissa Evans', 100)]
-# [('Eric Villegas', 100), ('Melissa Evans', 100)]
+# print(top_k_scores(ippt_takers_data, 5, 25))    # [('Joseph Burns', 100), ('Mike Williams', 98), ('Rachel Serrano', 98), ('Margaret Jennings', 97), ('Alexandra Day', 95), ('Stephen Boyer', 95)]
+# print(top_k_scores(ippt_takers_data, 1, 28))    # [('Eric Villegas', 100), ('Melissa Evans', 100)]
+# print(top_k_scores(ippt_takers_data, 2, 28))    # [('Eric Villegas', 100), ('Melissa Evans', 100)]
